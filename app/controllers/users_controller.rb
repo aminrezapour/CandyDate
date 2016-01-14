1ae666847a26719fd9e17a3343d1f62844f0271b
Amin Rezapour <aminrezapour@gmail.com>
2016-01-14 11:56:01 -0800

    Add telephone to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(current_user)
   end
+
+  def update
+    @user = current_user
+    @user.update_attribute(:telephone, params[:user][:telephone])
+    redirect_to(@user) if @user.save
+  end
 end
